70b073ccdcac04d4d3e51bc205b92f6bdf270eec
Matheus Cruz <bromatheuscruz@gmail.com>
2019-07-13 15:53:23 -0300

    Adicionando url para dev

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://odont-api.herokuapp.com/api/rest/";
+export const API_URL = "http://localhost:3000/api/rest/";
 export const API_VIA_CEP_URL = "https://viacep.com.br/ws/${cep}/json/ "
